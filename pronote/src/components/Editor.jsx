6768138dbca00ed6c6df9fccbdb373717a3f114c
jithu2023 <jithumon.j@kalvium.community>
2023-12-20 15:52:07 +0530

    updated

---
+++ pronote/src/components/Editor.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+
+class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="input">
+                    <h3>Input</h3>
+                    <textarea
+                        className="input-text"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="output">
+                    <h3>Pro Note</h3>
+                    <div className="output-text">{this.state.value}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Editor;
